79dbad443a6f3a00f30c52b045eb87565076eed5
John Froi <johnfroi47@gmail.com>
2024-11-12 16:35:36 +0800

    form move, add and delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,17 +16,20 @@ import { User } from './interfaces';
 export class AppComponent {
   store = inject(Store);
   fb = inject(FormBuilder);
-  form!: FormArray;
+  form!: FormArray<FormGroup>;
 
   constructor() {
     this.store.select(selectUsers)
       .subscribe((users) => {
-        console.log(users);
         this.initializeForm(users);
       });
   }
 
   initializeForm(users: User[]) {
+    if (users.length > 0) {
+      this.form = this.fb.array(users.map(user => this.buildFormGroup(user)));
+    }
+
     const placeholder = this.buildFormGroup({
       id: 0,
       firstName: '',
@@ -41,14 +44,39 @@ export class AppComponent {
     ]);
   }
 
-  buildFormGroup(user: User): FormGroup {
+  buildFormGroup(user?: User): FormGroup {
     return this.fb.group({
-      id: [user.id],
-      firstName: [user.firstName, [Validators.required]],
-      lastName: [user.lastName, [Validators.required]],
-      dropDown: [user.dropDown, [Validators.required]],
-      position: [user.position]
+      id: [user?.id ?? 0],
+      firstName: [user?.firstName, [Validators.required]],
+      lastName: [user?.lastName, [Validators.required]],
+      dropDown: [user?.dropDown, [Validators.required]],
+      position: [user?.position]
     });
+  }
+
+  addNewForm(index: number) {
+    this.form.insert(index + 1, this.buildFormGroup());
+  }
+
+  deleteForm(index: number) {
+    this.form.removeAt(index);
+  }
+
+  moveUp(index: number) {
+    const currentForm = this.form.controls[index];
+    const targetForm = this.form.controls[index - 1];
+
+    this.form.controls[index - 1] = currentForm;
+    this.form.controls[index] = targetForm;
+  }
+
+  moveDown(index: number) {
+    const newIndex = index + 1;
+    const currentForm = this.form.controls[index];
+    const targetForm = this.form.controls[newIndex];
+
+    this.form.controls[newIndex] = currentForm;
+    this.form.controls[index] = targetForm;
   }
 
   addUser(userForm: FormGroup) {
@@ -71,5 +99,9 @@ export class AppComponent {
     }
     
     this.store.dispatch(userActions.update({payload: userForm.value}));
+  }
+
+  trackByUserId(_: number, userForm: FormGroup) {
+    return userForm.value.id;
   }
 }
